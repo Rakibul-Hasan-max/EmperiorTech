09a714aece07b61f823448993db13381db0cc24b
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2021-10-04 12:42:41 +0600

    new push

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
+import About from './Components/About/About';
+import Blogs from './Components/Blogs/Blogs';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
+import NotFound from './Components/NotFound/NotFound';
+import Services from './Components/Services/Services';
 
 function App() {
   return (
-    <div className="App">
-      <h1>EmperiorTech</h1>
-    </div>
+  <Router>
+    <Header></Header>
+    <Footer></Footer>
+    <Switch>
+      <Route exact path="/">
+        <Home></Home>
+      </Route>
+      <Route exact path="/home">
+        <Home></Home>
+      </Route>
+      <Route path="/about">
+        <About></About>
+      </Route>
+      <Route path="/services">
+        <Services></Services>
+      </Route>
+      <Route path="/blogs">
+        <Blogs></Blogs>
+      </Route>
+      <Route path="/*">
+        <NotFound></NotFound>
+      </Route>
+    </Switch>
+  </Router>
   );
 }
 
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>this is header</h1>
+            <button>Home</button>
+            <button>About</button>
+            <button>Services</button>
+            <button>Blogs</button>
+        </div>
+    );
+};
+
+export default Header;
