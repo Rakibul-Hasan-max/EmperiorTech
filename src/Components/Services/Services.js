6b2f727dc7a5eef43cb244c415c251c35277c8b4
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2021-10-04 22:06:51 +0600

    card design

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './Services.css';
 
 const Services = () => {
@@ -7,8 +9,44 @@ const Services = () => {
             <h1>Services</h1>
             <hr />
             <p>We harness the strength of our multi-disciplinary teams to craft the perfect solutions for your business.</p>
+            <Lefts></Lefts>
         </div>
     );
 };
+
+
+const Lefts = () => {
+  const [lefts, setLefts] = useState([]);
+  useEffect( () => {
+    fetch('./Blogs.JSON')
+    .then(res => res.json())
+    .then(data => setLefts(data));
+  } ,[])
+
+  return(
+    <div className="left">
+        {
+            lefts.map(left => <Left left={left} key={left.team}></Left>)
+        }
+      
+    </div>
+  )
+}
+
+
+const Left = (props) => {
+    console.log(props.left);
+    const {name, team, price, description, content} =  props.left;
+    return (
+        <div>
+            <h2>Name: {name}</h2>
+            <p>Team: {team}</p>
+            <h5>Price: {price}</h5>
+            <p>Description: {description}</p>
+            <p>Content: {content}</p>
+        </div>
+    );
+};
+
 
 export default Services;
