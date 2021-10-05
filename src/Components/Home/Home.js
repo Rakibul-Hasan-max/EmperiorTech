b71b688bd5d677d4156e5ed05b65bc9e7cca94e8
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2021-10-05 18:43:56 +0600

    new done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -23,6 +23,33 @@ const Home = () => {
             </div>
             <h1 className="text-center m-5">Our Services</h1>
             <Lefts></Lefts>
+            <div>
+                <div className="container marketing">
+                    <h1 className="text-center mt-5 mb-5">Our Clients</h1>
+                    <div className="row mb-5">
+                        <div className="col-lg-4 text-center">
+                            <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+                            <h2>John Michel</h2>
+                            <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
+                            <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
+                        </div> 
+                        <div class="col-lg-4 text-center">
+                            <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+
+                            <h2>Henry Aiden</h2>
+                            <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
+                            <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
+                        </div> 
+                        <div class="col-lg-4 text-center">
+                            <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+
+                            <h2>Lezel Burg</h2>
+                            <p>And lastly this, the third column of representative placeholder content.</p>
+                            <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
+                        </div> 
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
